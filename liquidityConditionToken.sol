fc29ee091ee6af40db45123f93a9b19bd4c5fd82
abdulch47 <110175204+abdulch47@users.noreply.github.com>
2024-07-18 10:25:22 +0500

    Add files via upload

---
+++ liquidityConditionToken.sol
@@ -0,0 +1,274 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        return c;
+    }
+}
+
+contract Ownable is Context {
+    address private _owner;
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    constructor() {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+}
+
+interface ITokenPresale {
+    struct PresaleInfo {
+        uint256 totalTokens;
+        uint256 tokenPrice;
+        uint256 totalSold;
+        uint256 bnbCollected;
+        uint256 usdtCollected;
+        uint256 usdcCollected;
+        uint256 phasetime;
+    }
+
+    function presalePhases(uint256 index) external view returns (
+        uint256 totalTokens,
+        uint256 tokenPrice,
+        uint256 totalSold,
+        uint256 bnbCollected,
+        uint256 usdtCollected,
+        uint256 usdcCollected,
+        uint256 phasetime
+    );
+}
+
+contract PlayStation is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address => bool) private isLpPair;
+    mapping(address => bool) private isDex;
+
+    address private liquidityOwner;
+
+    uint8 private constant _decimals = 18;
+    uint256 private constant _tTotal = 10000000000 * 10**_decimals;
+    string private constant _name = "PlayStation Token";
+    string private constant _symbol = "PlayStation";
+
+    bool public tradingOpen = false;
+
+    constructor(address _liquidityOwner) {
+        liquidityOwner = _liquidityOwner;
+        _balances[_msgSender()] = _tTotal;
+        emit Transfer(address(0), _msgSender(), _tTotal);
+    }
+
+    function name() public pure returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public pure returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public pure override returns (uint256) {
+        return _tTotal;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount)
+        public
+        override
+        returns (bool)
+    {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount)
+        public
+        override
+        returns (bool)
+    {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(
+            sender,
+            _msgSender(),
+            _allowances[sender][_msgSender()].sub(
+                amount,
+                "ERC20: transfer amount exceeds allowance"
+            )
+        );
+        return true;
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) private {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) private {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        if (from != owner() && to != owner()) {
+            if (!tradingOpen && tx.origin != liquidityOwner) {
+                require(!isDex[msg.sender], "You can't add liquidity");
+            }
+        }
+        _balances[from] = _balances[from].sub(amount);
+        _balances[to] = _balances[to].add(amount);
+        emit Transfer(from, to, amount);
+    }
+
+    function openTrading() external onlyOwner {
+        require(!tradingOpen, "trading is already open");
+        tradingOpen = true;
+    }
+
+    function setDex(address[] memory dexAddresses) external onlyOwner {
+        for (uint8 i; i < dexAddresses.length; i++) {
+            isDex[dexAddresses[i]] = true;
+        }
+    }
+
+    receive() external payable {}
+
+    function withDrawETH() external onlyOwner {
+        require(address(this).balance > 0, "Not enough eth");
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function withdrawStuckTokens() external onlyOwner {
+        uint256 balance = balanceOf(address(this));
+        require(balance > 0, "No balance to withdraw");
+        _transfer(address(this), owner(), balance);
+    }
+}
